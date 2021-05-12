58d97cb46d5e67a1f914f625890a3b9cf71fe80d
wz <wangzhen@huobi.com>
2021-05-12 20:24:37 +0800

    这里还没搞懂   得在看看 先回家吧

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 //导航到新路由
                 Navigator.push( context,
                     MaterialPageRoute(builder: (context) {
-                      return NewRoute();
+                      return RouterTestRoute();
+                      // return TipRoute(text:'fasdfadf ');
                     }));
               },
             ),
@@ -122,15 +123,60 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class NewRoute extends StatelessWidget {
+
+
+class TipRoute extends StatelessWidget {
+  TipRoute({
+    Key key,
+    @required this.text,  // 接收一个text参数
+  }) : super(key: key);
+  final String text;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('new Route')
+        title: Text("提示"),
       ),
-      body: Center(
-        child: Text('this is new route'),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, "我是返回值"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class RouterTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () async {
+          // 打开`TipRoute`，并等待返回结果
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return TipRoute(
+                  // 路由参数
+                  text: "我是提示xxljkljlkxx",
+                );
+              },
+            ),
+          );
+          //输出`TipRoute`路由返回结果
+          print("路由返回值: $result");
+        },
+        child: Text("打开提示页"),
       ),
     );
   }
